44573bbbb407183498b4909201d5fd123fe4353e
nana <nrdcmobile@gmail.com>
2019-02-14 16:53:09 +0330

    refactor code and add readline

--- import.js
+++ import.js
@@ -1,9 +1,19 @@
+const helper = require("./helper");
+const readline = require("readline");
 const fs = require("fs");
 const mysql = require("mysql");
-const configuration = { directory: "db-backup", password: "13@sMz&77" };
-import { parseArgs } from "./helper";
+const configuration = helper();
 const basePath = `./${configuration.directory}`;
-parseArgs();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Enter Password : \n", answer => {
+  configuration.password = answer;
+  r1.close();
+});
+// configuration.password = password;
+
 let connection = mysql.createConnection(configuration);
 connection.connect();
 fs.readdir(basePath, (err, files) => {
@@ -12,8 +22,7 @@ fs.readdir(basePath, (err, files) => {
     if (file.endsWith(".data.json")) {
       let rawData = fs.readFileSync(basePath + "/" + file);
       let jsonObject = JSON.parse(rawData);
-      if(!file.endsWith(".data.json"))
-      continue ;
+      if (!file.endsWith(".data.json")) return;
       let query = `INSERT INTO \`${configuration.database}\`.\`${file.slice(
         0,
         file.indexOf(".data.json")
@@ -22,7 +31,7 @@ fs.readdir(basePath, (err, files) => {
       if (jsonObject.length > 1)
         Object.keys(jsonObject[0]).forEach(col => colmuns.push(col));
       colmuns.forEach(col => (query += `\`${col}\` ,`));
-      query = query.slice(0, - 1);
+      query = query.slice(0, -1);
       query += `) \n`;
       query += "VALUES (";
       jsonObject.forEach(item => {
@@ -30,12 +39,12 @@ fs.readdir(basePath, (err, files) => {
         colmuns.forEach(col => {
           query2 += `'${item[col]}' ,`;
         });
-        query2 = query2.slice(0, - 1);
+        query2 = query2.slice(0, -1);
         query2 += `) ;`;
         console.log(query2);
         connection.query(query2, function(error, rows, fields) {
           if (error) throw error;
-            console.log(rows);
+          console.log(rows);
         });
       });
     }
