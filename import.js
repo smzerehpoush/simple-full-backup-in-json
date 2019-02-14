bbf35baeab944884e5101a44ed0de2d51506ae5b
nana <nrdcmobile@gmail.com>
2019-02-15 01:03:14 +0330

    implement logic of adding bit from buffer data

--- import.js
+++ import.js
@@ -41,6 +41,8 @@ function insertToDB() {
           let d = item[col]
           if (col.includes("_date") && item[col].length === 24) {
             query2 += `'${d.slice(0,10)} ${d.slice(11,19)}' ,`;
+          } else if (item[col]["type"] === 'Buffer') {
+            query2 += `b'${item[col]["data"][0]}' ,`
           } else query2 += `'${d}' ,`;
         });
         query2 = query2.slice(0, -1);
